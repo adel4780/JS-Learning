fba7dd6071a9797b534441b846922dd4f6b9f476
adel4780 <adelk138019@gmail.com>
2024-03-15 17:21:47 +0330

    Delete extra folders

--- JS/26-getElementById/script.js
+++
@@ -1,7 +0,0 @@
-var myButton = document.getElementById('b1');
-
-var demoParagraph= document.getElementById('demo');
-
-myButton.addEventListener('click', function(e) {
-    demoParagraph.innerHTML="Hello";
-});
